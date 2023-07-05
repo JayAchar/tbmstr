e1c406e00ae4555e8ab7a4efdff4800ab1f26301
Jay Achar <17378875+JayAchar@users.noreply.github.com>
2023-07-05 23:03:28 +0200

    46-add-example-scripts (#50)
    * feat(scripts): add example analysis script to create table 1
    
    Closes #46
    
    * feat(scripts): add table 2 to example script
    
    Closes #49

---
+++ scripts/example_analysis.R
@@ -0,0 +1,105 @@
+library(tbmstr)
+suppressMessages(library(gtsummary))
+suppressMessages(library(dplyr))
+library(gt)
+library(broom)
+
+# change parent_dir as required
+parent_dir <- here::here("data", "parent_dir")
+output_dir <- here::here("data", "output")
+
+# read in and prepare data
+raw <- run_analysis(
+  parent_dir = parent_dir,
+  file_names = list(
+    baseline = "baseline",
+    adverse = "adverse"
+  ),
+  data_only = TRUE
+)
+
+df <- raw$baseline |>
+  mutate(
+    had_sae = factor(if_else(
+      had_sae == TRUE, "Yes", "No"
+    ), levels = c("No", "Yes")),
+    is_employed = empl == "Employed"
+  )
+
+df |>
+  tbl_summary(
+    include = c(
+      age, sex, empl,
+      is_employed,
+      smok, hiv, diab,
+      cav, hbsag, hcvab,
+      outcome, had_sae,
+      stat12
+    ),
+    label = list(
+      age ~ "Age (years)",
+      sex ~ "Sex",
+      empl ~ "Employment status",
+      is_employed ~ "Is the participant employed?",
+      smok ~ "Smoking status",
+      hiv ~ "HIV status",
+      diab ~ "Diabetes",
+      cav ~ "Cavitatory disease",
+      hbsag ~ "HBsAg status",
+      hcvab ~ "HCV Ab status",
+      outcome ~ "End of treatment outcome",
+      had_sae ~ "Suffered a SAE",
+      stat12 ~ "Status 12m post-treatment completion"
+    ),
+    type = all_dichotomous() ~ "categorical",
+    missing = "ifany",
+    missing_text = "Missing"
+  ) |>
+  bold_labels() |>
+  as_gt() |>
+  gt::gtsave(
+    filename = "table_1.docx",
+    path = output_dir
+  )
+
+
+df |>
+  filter(hbsag != "Not done") |>
+  tbl_uvregression(
+    include = c(
+      tx_outcome,
+      age,
+      sex,
+      is_employed,
+      smok,
+      hiv,
+      diab,
+      cav,
+      hbsag,
+      hcvab
+    ),
+    label = list(
+      age ~ "Age (years)",
+      sex ~ "Sex",
+      empl ~ "Employment status",
+      is_employed ~ "Is the participant employed?",
+      smok ~ "Smoking status",
+      hiv ~ "HIV status",
+      diab ~ "Diabetes",
+      cav ~ "Cavitatory disease",
+      hbsag ~ "HBsAg status",
+      hcvab ~ "HCV Ab status"
+    ),
+    method = glm,
+    method.args = list(family = binomial(link = "logit")),
+    exponentiate = TRUE,
+    y = tx_outcome == "Success",
+    add_estimate_to_reference_rows = TRUE
+  ) |>
+  add_global_p() |>
+  bold_labels() |>
+  as_gt() |>
+  gt::gtsave(
+    filename = "table_2.docx",
+    path = output_dir
+  )
