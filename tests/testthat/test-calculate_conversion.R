b2de730b228d473dbb1d3bbfb7772f43989529f4
J Achar <17378875+JayAchar@users.noreply.github.com>
2023-08-08 10:40:45 +0200

    feat(mutate): add low level calculate_conversion function
    #60

---
+++ tests/testthat/test-calculate_conversion.R
@@ -0,0 +1,95 @@
+test_that("error on incorrect inputs", {
+  expect_error(
+    calculate_conversion(list())
+  )
+
+  expect_error(
+    calculate_conversion(
+      data.frame(
+        id = c(1, 2),
+        date = as.Date(c(1, 2)),
+        result = c(0, 1)
+      )
+    )
+  )
+
+  expect_error(
+    calculate_conversion(
+      data.frame(
+        id = c(1, 1),
+        date = as.Date(c(1, 2)),
+        result = c(0, 4)
+      )
+    )
+  )
+})
+
+test_that("conversion check works", {
+  input <- data.frame(
+    id = c(1, 1),
+    date = as.Date(c(1, 50)),
+    result = c(0, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(50)
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  expect_equal(observed, expected)
+})
+
+test_that("adjusting tolerance works", {
+  input <- data.frame(
+    id = c(1, 1, 1),
+    date = as.Date(c(1, 29, 50)),
+    result = c(0, 0, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(50)
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  adjusted <- calculate_conversion(
+    subject_df = input,
+    tolerance = 28
+  )
+
+  expect_equal(observed, expected)
+
+  expect_equal(adjusted, data.frame(
+    id = 1,
+    date = as.Date(29)
+  ))
+})
+
+test_that("handles positive results", {
+  input <- data.frame(
+    id = c(1, 1, 1),
+    date = as.Date(c(1, 10, 50)),
+    result = c(1, 0, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(50)
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  expect_equal(observed, expected)
+})
