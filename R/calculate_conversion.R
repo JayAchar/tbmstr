abcbd7688993104c168111b330d53edba619b7b8
Jay Achar <17378875+JayAchar@users.noreply.github.com>
2023-08-09 20:17:57 +0200

    Add conversion date calculating function (#61)
    * feat(mutate): add low level calculate_conversion function
    
    #60
    
    * feat(analysis): add function to calculate conversion date for individual subjects
    
    Closes #60
    
    * fix(analysis): fix test error for as.Date in R 4.2 where origin is required
    
    #60

---
+++ R/calculate_conversion.R
@@ -0,0 +1,86 @@
+#' Calculate conversion date
+#'
+#' @param subject_df data frame with ID, date and result variables
+#' @param tolerance integer to represent number of days between negative
+#' results which constitute conversion
+#'
+#' @return data frame with subject ID and conversion date variable
+#'
+#' @examples
+calculate_conversion <- function(subject_df,
+                                 tolerance = 30L) {
+  stopifnot(
+    is.data.frame(subject_df),
+    all(c("id", "date", "result") %in% names(subject_df)),
+    length(unique(subject_df$id)) == 1L,
+    all(subject_df$result %in% c(0, 1)),
+    all(!is.na(subject_df$date))
+  )
+
+  # capture number of results
+  result_sequence <- seq_len(nrow(subject_df))
+
+  # add days_after_previous_negative variable
+  diffs <- vapply(
+    X = result_sequence,
+    FUN = function(row) {
+      if (row == 1 || subject_df[row, "result"] == 1) {
+        return(0)
+      }
+
+      # if the difference is less than the tolerance, check the previous
+      # row.
+
+      # For each result, recurse backwards to find the longest sequence of
+      # negative results then calculate the number of days
+
+      days_diff <- Reduce(
+        x = row:min(result_sequence),
+        f = function(res, inc) {
+          if (inc == 1) {
+            return(res)
+          }
+          if (subject_df[inc, "result"] == 1) {
+            return(0)
+          }
+          # stop recursing back if the current diff since negative > required
+          if (res >= tolerance) {
+            return(res)
+          }
+          # if we encounter a positive result when recursing back,
+          # reset the value
+          if (subject_df[inc - 1, "result"] == 1) {
+            return(0)
+          }
+          # if the recursed result is also negative, add accumulate the
+          # additional diff_days
+          if (subject_df[inc - 1, "result"] == 0) {
+            diff <- as.numeric(subject_df[row, "date"] -
+              subject_df[row - 1, "date"])
+            return(diff + res)
+          }
+        },
+        init = 0
+      )
+      return(days_diff)
+    },
+    FUN.VALUE = double(1)
+  )
+
+  subject_df$diff <- diffs
+  # retain candidate results which might represent conversion
+  candidates <- subject_df[which(subject_df$diff >= tolerance), ]
+
+  if (nrow(candidates) == 0) {
+    conversion_date <- as.Date(NA)
+  } else {
+    conversion_date <- min(candidates$date)
+  }
+
+  return(
+    data.frame(
+      id = unique(subject_df$id),
+      date = conversion_date
+    )
+  )
+}
---
+++ tests/testthat/test-calculate_conversion.R
@@ -0,0 +1,165 @@
+test_that("error on incorrect inputs", {
+  expect_error(
+    calculate_conversion(list())
+  )
+
+  expect_error(
+    calculate_conversion(
+      data.frame(
+        id = c(1, 2),
+        date = as.Date(c(1, 2), origin = "1970-01-01"),
+        result = c(0, 1)
+      )
+    )
+  )
+
+  expect_error(
+    calculate_conversion(
+      data.frame(
+        id = c(1, 1),
+        date = as.Date(c(1, 2), origin = "1970-01-01"),
+        result = c(0, 4)
+      )
+    )
+  )
+
+  expect_error(
+    calculate_conversion(
+      data.frame(
+        id = c(1, 1),
+        date = as.Date(c(1, NA), origin = "1970-01-01"),
+        result = c(0, 1)
+      )
+    )
+  )
+})
+
+test_that("conversion check works", {
+  input <- data.frame(
+    id = c(1, 1),
+    date = as.Date(c(1, 50), origin = "1970-01-01"),
+    result = c(0, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(50, origin = "1970-01-01")
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  expect_equal(observed, expected)
+})
+
+test_that("adjusting tolerance works", {
+  input <- data.frame(
+    id = c(1, 1, 1),
+    date = as.Date(c(1, 29, 50), origin = "1970-01-01"),
+    result = c(0, 0, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(50, origin = "1970-01-01")
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  adjusted <- calculate_conversion(
+    subject_df = input,
+    tolerance = 28
+  )
+
+  expect_equal(observed, expected)
+
+  expect_equal(adjusted, data.frame(
+    id = 1,
+    date = as.Date(29, origin = "1970-01-01")
+  ))
+})
+
+test_that("handles positive results", {
+  input <- data.frame(
+    id = c(1, 1, 1),
+    date = as.Date(c(1, 10, 50), origin = "1970-01-01"),
+    result = c(1, 0, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(50, origin = "1970-01-01")
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  expect_equal(observed, expected)
+})
+
+test_that("handles terminal positive results", {
+  input <- data.frame(
+    id = c(1, 1, 1),
+    date = as.Date(c(1, 10, 50), origin = "1970-01-01"),
+    result = c(0, 1, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(NA, origin = "1970-01-01")
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  expect_equal(observed, expected)
+})
+
+test_that("return earliest conversion", {
+  input <- data.frame(
+    id = c(1, 1, 1, 1, 1),
+    date = as.Date(c(1, 35, 40, 45, 90), origin = "1970-01-01"),
+    result = c(0, 0, 1, 0, 0)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(35, origin = "1970-01-01")
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  expect_equal(observed, expected)
+})
+
+test_that("handles all positive", {
+  input <- data.frame(
+    id = c(1, 1, 1),
+    date = as.Date(c(1, 35, 40), origin = "1970-01-01"),
+    result = c(1, 1, 1)
+  )
+
+  expected <- data.frame(
+    id = 1,
+    date = as.Date(NA, origin = "1970-01-01")
+  )
+
+  observed <- calculate_conversion(
+    subject_df = input,
+    tolerance = 30
+  )
+
+  expect_equal(observed, expected)
+})
