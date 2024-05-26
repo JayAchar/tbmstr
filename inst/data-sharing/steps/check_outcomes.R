41d7842e115ca2c8f0bcfaf5953e6f45523fd9ba
J Achar <17378875+JayAchar@users.noreply.github.com>
2024-05-27 00:01:48 +0200

    feat(outcomes): update completed outcomes to cured if criteria fulfilled
    MSTR-43

--- inst/data-sharing/steps/check_outcomes.R
+++ inst/data-sharing/steps/check_outcomes.R
@@ -16,15 +16,31 @@ check_outcomes <- function(lst) {
     by = "globalrecordid"
   )
 
-  merge(
+  binary_df <- merge(
     outcomes,
     is_baseline_confirmed,
     all.x = TRUE,
     by = "globalrecordid"
   )
 
-  # FIXME: none of the logic in this function is applied to the main line of
-  # analysis
+  binary_df$is_cured <- binary_df$has_cultures & binary_df$is_baseline_positive
+
+  baseline <- merge(
+    lst$baseline,
+    binary_df[, c("globalrecordid", "is_cured")],
+    all.x = TRUE,
+    by = "globalrecordid"
+  )
+
+  # FIXME: the outcome variable is changed to a numeric variable here
+  baseline$outcome <- ifelse(!is.na(baseline$is_cured),
+    baseline$is_cured,
+    baseline$outcome
+  )
+
+  baseline$is_cured <- NULL
+
+  lst$baseline <- baseline
 
   return(lst)
 }
